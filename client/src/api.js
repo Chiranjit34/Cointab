4bc7aab56a482c6042a5b30ca94dfb77996c320a
Chiranjit Behera <chiranjitbehera34@gmail.com>
2022-12-02 17:35:03 +0530

    backend uplaoded to heroku

--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,4 @@
-export const api = "http://localhost:8080";
+// export const api = "http://localhost:8080";
+export const api = "https://thawing-basin-68870.herokuapp.com";
 export const register = `${api}/users/register`;
 export const login = `${api}/users/login`;
